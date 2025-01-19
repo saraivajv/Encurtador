d334f7894ac046c663cc2851b1b3bcd056ca3d80
Pedro V. Pinho <pvpedrinho06@gmail.com>
2025-01-19 19:10:57 -0300

    Ajustes finais

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
-use actix_web::error;
-use actix_web::{web, App, HttpResponse, HttpServer, Responder, Result};
+use std::env;
+
+use actix_web::{
+    error::{self, ErrorBadRequest, ErrorInternalServerError},
+    get,
+    http::header::ContentType,
+    post,
+    web::{self},
+    App, HttpResponse, HttpServer, Responder, Result,
+};
+use anyhow::Context;
+use dotenv::dotenv;
 use rand::{distributions::Alphanumeric, Rng};
 use serde::{Deserialize, Serialize};
-use sqlx::migrate::MigrateDatabase;
-use sqlx::{Sqlite, SqlitePool};
+use sqlx::{migrate::MigrateDatabase, Sqlite, SqlitePool};
+use url::Url;
 
 #[derive(Deserialize)]
 struct UrlInput {
@@ -13,6 +23,10 @@ struct UrlInput {
 #[derive(Serialize)]
 struct UrlOutput {
     short_url: String,
+}
+
+struct UrlRecord {
+    url: String,
 }
 
 struct AppState {
@@ -27,88 +41,85 @@ fn generate_code(length: usize) -> String {
         .collect()
 }
 
+#[post("/encurtar")]
 async fn shorten_url(
     data: web::Data<AppState>,
     input: web::Json<UrlInput>,
 ) -> Result<impl Responder> {
     let code = generate_code(6);
 
-    match sqlx::query("INSERT INTO urls VALUES (?, ?)")
-        .bind(&code)
-        .bind(&input.url)
+    let url = Url::parse(&input.url).map_err(ErrorBadRequest)?.to_string();
+
+    let query = sqlx::query!("INSERT INTO urls VALUES (?, ?)", code, url)
         .execute(&data.db)
-        .await
-    {
+        .await;
+
+    match query {
         Ok(_) => {
             let short_url = format!("http://localhost:8080/{code}");
             Ok(web::Json(UrlOutput { short_url }))
         }
-        Err(e) => Err(error::ErrorInternalServerError(e)),
+        Err(e) => Err(ErrorInternalServerError(e)),
     }
 }
 
+#[get("/{code}")]
 async fn redirect(data: web::Data<AppState>, path: web::Path<String>) -> Result<impl Responder> {
     let code = path.into_inner();
 
-    let a = sqlx::query_as::<_, (String,)>("SELECT url FROM urls WHERE url_hash = ?")
-        .bind(code)
+    let query = sqlx::query_as!(UrlRecord, "SELECT url FROM urls WHERE url_hash = ?", code)
         .fetch_optional(&data.db)
         .await;
 
-    match a {
-        Ok(Some(url)) => Ok(HttpResponse::Found()
-            .append_header(("location", url.0))
+    match query {
+        Ok(Some(v)) => Ok(HttpResponse::TemporaryRedirect()
+            .append_header(("location", v.url))
             .finish()),
-        Ok(None) => Ok(HttpResponse::NotFound().body("URL não encontrada")),
+        Ok(None) => Ok(HttpResponse::NotFound()
+            .content_type(ContentType::plaintext())
+            .body("URL não encontrada")),
         Err(e) => Err(error::ErrorInternalServerError(e)),
     }
 }
 
-const DATABASE_URL: &str = "sqlite:encurtador.db";
-
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    if !Sqlite::database_exists(DATABASE_URL).await.unwrap_or(false) {
-        println!("Criando banco de dados `{DATABASE_URL}`");
-        match Sqlite::create_database(DATABASE_URL).await {
-            Ok(()) => println!("Banco de dados criado com sucesso!"),
-            Err(e) => panic!("Erro ao criar banco de dados: {e}"),
-        }
+async fn main() -> anyhow::Result<()> {
+    dotenv().ok();
+
+    let db_url = env::var("DATABASE_URL")
+        .context("Variável de ambiente `DATABASE_URL` não está definida")?;
+
+    if !Sqlite::database_exists(&db_url).await.unwrap_or(false) {
+        println!("Criando banco de dados `{db_url}`");
+
+        Sqlite::create_database(&db_url)
+            .await
+            .context("Erro ao criar banco de dados")?;
     }
 
-    let db = match SqlitePool::connect(DATABASE_URL).await {
-        Ok(pool) => pool,
-        Err(e) => {
-            panic!("Erro ao criar tabela do banco de dados: {e}");
-        }
-    };
-
-    let crate_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
-    let migrations = std::path::Path::new(&crate_dir).join("./migrations");
-
-    let migration_results = sqlx::migrate::Migrator::new(migrations)
+    let db = SqlitePool::connect(&db_url)
         .await
-        .unwrap()
-        .run(&db)
-        .await;
+        .with_context(|| format!("Erro ao conectar ao banco de dados `{db_url}`"))?;
 
-    if let Err(e) = migration_results {
-        panic!("error: {e}");
-    }
+    sqlx::migrate!()
+        .run(&db)
+        .await
+        .context("Erro ao criar tabela do banco de dados")?;
 
     let app_state = web::Data::new(AppState { db });
+
+    println!("Servidor rodando em http://localhost:8080");
 
     HttpServer::new(move || {
         App::new()
             .app_data(app_state.clone())
-            .route("/encurtar", web::post().to(shorten_url))
-            .route("/{code}", web::get().to(redirect))
+            .service(shorten_url)
+            .service(redirect)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1", 8080))
+    .context("Porta 8080 já está em uso")?
     .run()
     .await?;
-
-    println!("Servidor rodando em http://localhost:8080");
 
     Ok(())
 }
